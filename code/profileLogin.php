a7480dac7b1d09d3ffc49c77e9b589b85e9b6416
Nishant Rai <nishantr@iitk.ac.in>
2017-04-13 15:29:51 +0530

    Fixed bugs in profile login

--- code/profileLogin.php
+++ code/profileLogin.php
@@ -3,7 +3,7 @@
 $sname = "127.0.0.1";
 $uname = "root";
 $pwd = "db.123";
-$dbname = "cs252test"
+$dbname = "cs252test";
 
 $connect = mysql_connect($sname,$uname,$pwd);
 
@@ -17,18 +17,16 @@ $userPass = $_POST['pass'];
 //Choose the required database 
 mysql_select_db($dbname);
 
-echo "HERE\n"
-
 // Extract the decrypted passwords and usernames
 // Note that the table contains entries 'username' and 'password'
-$query = "select AES_DECRYPT(password, 'some_secret_key') as password, username from profLogin"
+$query = "SELECT aes_decrypt(password, 'some_secret_key') AS password, username FROM profLogin";
 $result = mysql_query($query, $connect);
 
 if(! $result) {
     die("Some error in profileLogin.php!\n" . mysql_error());
 }
 
-// Get the number of rows in the table
+// Get the number of rows in the result
 $num_rows = mysql_num_rows($result);
  
 //Loop through the rows of table to check the password 
@@ -50,14 +48,12 @@ while($num_rows > 0) {
  
  }
 
-// if($flag == 1)
-//     echo "<font color='green'> Successful! </font>";
-// else
-//     echo "<font color='red'> Unsuccessful! </font>";
+if($flag == 1)
+    echo "<font color='green'> Successful Login! </font>";
+else
+    echo "<font color='red'> Unsuccessful Login! </font>";
 
-echo "FINISHED!\n";
-
-echo "<br><center><form action='index.html' method='post'> <input type='submit' value='Go Back to Main Page'></form></center>";
+echo "<br><br><br><center><form action='index.html' method='post'> <input type='submit' value='Go Back to Main Page'></form></center>";
 
 mysql_close($connect);
 
