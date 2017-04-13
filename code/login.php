48308814af5d5c6bfcd9c480b4f93484c8d7b3b6
Amlan Kar <amlan.asaa@gmail.com>
2017-04-13 16:49:01 +0530

    Login.php

--- code/login.php
+++ code/login.php
@@ -5,19 +5,28 @@ $pass=($_POST['pass']);
 
 if (@ftp_login(@ftp_connect("vyom.cc.iitk.ac.in"), $id, $pass))
 {
-	if (array_key_exists('id', $_SESSION) && $_SESSION['id'] == $id) 
-	{
-		echo"<script type='text/javascript'>alert('User already logged in!');</script>";
-	}
+    if (array_key_exists('id', $_SESSION))
+    {
+        if($_SESSION['id'] == $id){ 
+        echo"<script type='text/javascript'>alert('User already logged in!'); 
+             window.location.href='dash.html';</script>";
+        }
 
-	else
-	{
-		session_destroy();
-		session_start();
-		$_SESSION['id'] = $id;
-	}
-	echo"Wonderful! Login successful!";	
+        else{
+        echo"<script type='text/javascript'>alert('Some other user is logged in!');
+             window.location.href='login.html';</script>";
+        die();
+        }
+    }
+
+    else
+    {
+        session_destroy();
+        session_start();
+        $_SESSION['id'] = $id;
+    }
 }
+
 else 
 {
 	echo"Invalid username or password";
