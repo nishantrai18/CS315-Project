d5381b42253452ffc117ad39be98417e99939993
Nishant Rai <nishantr@iitk.ac.in>
2017-04-13 14:21:09 +0530

    Profile login added

--- code/login.php
+++ code/login.php
@@ -3,15 +3,11 @@ session_start();
 $id=$_POST['id'];
 $pass=($_POST['pass']);
 
-include_once("header.php");
-
 if (@ftp_login(@ftp_connect("vyom.cc.iitk.ac.in"), $id, $pass))
 {
 	if (array_key_exists('id', $_SESSION) && $_SESSION['id'] == $id) 
 	{
 		echo"<script type='text/javascript'>alert('User already logged in!');</script>";
-		include_once("search.php");
-		//Header( "Location: search.php" );
 	}
 
 	else
@@ -19,8 +15,6 @@ if (@ftp_login(@ftp_connect("vyom.cc.iitk.ac.in"), $id, $pass))
 		session_destroy();
 		session_start();
 		$_SESSION['id'] = $id;
-		include_once("search.php");
-		//Header( "Location: search.php" );
 	}
 	echo"Wonderful! Login successful!";	
 }
